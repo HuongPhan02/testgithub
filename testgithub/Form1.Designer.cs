993a8dc258d7228bf4b135ebb8f4903a8633ec89
Huong Phan <huong.ptc02@gmail.com>
2022-10-28 11:27:22 +0700

    Update form

--- testgithub/Form1.Designer.cs
+++ testgithub/Form1.Designer.cs
@@ -28,13 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btCong = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtSoA = new System.Windows.Forms.Label();
+            this.txtSoB = new System.Windows.Forms.Label();
+            this.txtKetQua = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btCong
+            // 
+            this.btCong.AutoSize = true;
+            this.btCong.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCong.Location = new System.Drawing.Point(515, 284);
+            this.btCong.Name = "btCong";
+            this.btCong.Size = new System.Drawing.Size(83, 39);
+            this.btCong.TabIndex = 0;
+            this.btCong.Text = "Cong";
+            this.btCong.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(224, 86);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // txtSoA
+            // 
+            this.txtSoA.AutoSize = true;
+            this.txtSoA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoA.Location = new System.Drawing.Point(90, 91);
+            this.txtSoA.Name = "txtSoA";
+            this.txtSoA.Size = new System.Drawing.Size(65, 29);
+            this.txtSoA.TabIndex = 2;
+            this.txtSoA.Text = "So a";
+            // 
+            // txtSoB
+            // 
+            this.txtSoB.AutoSize = true;
+            this.txtSoB.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoB.Location = new System.Drawing.Point(90, 145);
+            this.txtSoB.Name = "txtSoB";
+            this.txtSoB.Size = new System.Drawing.Size(65, 29);
+            this.txtSoB.TabIndex = 2;
+            this.txtSoB.Text = "So b";
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.AutoSize = true;
+            this.txtKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKetQua.Location = new System.Drawing.Point(90, 201);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(105, 29);
+            this.txtKetQua.TabIndex = 2;
+            this.txtKetQua.Text = "Ket Qua";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(224, 140);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(279, 34);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(224, 196);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(279, 34);
+            this.textBox3.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.txtSoB);
+            this.Controls.Add(this.txtSoA);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btCong);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btCong;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txtSoA;
+        private System.Windows.Forms.Label txtSoB;
+        private System.Windows.Forms.Label txtKetQua;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
